5bdf445b498dd72ca9b036a8978beea164a6d769
aarzoochikkodi2k5 <aarzoochikkodi05@gmail.com>
2025-11-12 15:34:02 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,24 +3,32 @@ import { StoreContext } from "../../context/StoreContext";
 import "./FoodDisplay.css";
 import FoodItem from "../FoodItem/FoodItem";
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  console.log("Food list:", food_list);
+
+  const filteredList = food_list.filter(
+    item => category === "All" || category === item.category
+  );
+
   return (
     <div className="food-display" id="food-display">
-        <h1>Top dishes near you</h1>
-        <div className="food-display-list">
-            {
-                food_list.map((item, index)=>{
-                  if(category === "All" || category === item.category){
-                  return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} img={item.image}/>
-                }
-                }
-                    
-              )
-            }
-        </div>
+      <h1>Top dishes near you</h1>
+      <div className="food-display-list">
+        {filteredList.map(item => (
+          <FoodItem
+            key={item._id}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            img={item.image}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default FoodDisplay;
+
